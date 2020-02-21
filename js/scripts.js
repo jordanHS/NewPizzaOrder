7e75c86b66310833e6f18e7f8db6e854ab337360
Jordan Safford <jordansafford@gmail.com>
2020-02-21 13:38:21 -0800

    add js logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,33 @@ function Pizza (size, toppings) {
     this.toppings = toppings;
 }
 
+Pizza.prototype.cost = function() {
+   var price = parseInt(this.size);
+   if (this.toppings.length > 3) {
+   return parseInt(price) + (parseInt(this.toppings.length) - 3)
+   } else {
+       return price;
+   }
+}
+
+
+//User Interface
+$(document).ready(function() {
+    $("#order").submit(function(event) {
+      event.preventDefault();
+        var name = $("input#userName").val();
+        var address = $("input#address").val();
+        var pizzaSize = $("input:radio[name=pizzaSize]:checked").val();
+        var pizzaToppings = [];
+        console.log(pizzaToppings);
+        $("input:checkbox[name=pizzaTopping]:checked").each(function(){
+        var toppings = $(this).val();
+        pizzaToppings.push(toppings)
+        
+    })
+
+    var newPizza = new Pizza(pizzaSize, pizzaToppings.length);
+    
+    $("#message").show().text("Thanks for the order, " + name +  "! Your order cost $" + newPizza.cost() + " &  will be delivered to the following address: " + address + ". See you soon!")
+    });
+});
