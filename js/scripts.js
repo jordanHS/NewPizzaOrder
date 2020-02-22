ec1cfce346fe28f648d4cf7c979097804cd13d83
Jordan Safford <jordansafford@gmail.com>
2020-02-21 18:13:28 -0800

    add beverage constructor with total prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,8 @@ function Pizza (cost, toppings) {
 
 Pizza.prototype.total = function() {
    var price = parseInt(this.cost);
-//    console.log(price);
    if (this.toppings.length > 3) {
-   return price + (parseInt(this.toppings.length) - 3)
+   return price + (parseInt(this.toppings.length) - 3);
    } else {
        return price;
    }
@@ -16,6 +15,12 @@ Pizza.prototype.total = function() {
 function Beverage (cost) {
     this.cost = cost;
 }
+
+Beverage.prototype.total = function() {
+    var price = parseFloat(this.cost);
+    return price;
+}
+
 
 //User Interface
 $(document).ready(function() {
@@ -28,14 +33,17 @@ $(document).ready(function() {
         $("input:checkbox[name=pizzaTopping]:checked").each(function(){
         var toppings = $(this).val();
         pizzaToppings.push(toppings)
-        
-    })
+       })
+    
+       var drinkSize = $("input:radio[name=drinkSize]:checked").val();
 
     var newPizza = new Pizza(pizzaSize, pizzaToppings);
+    var newDrink = new Beverage(drinkSize);
     console.log(newPizza);
     console.log(newPizza.total());
+    console.log(newDrink);
     
-    $("#message").show().text("Thanks for the order, " + name +  "! Your order cost $" + newPizza.total() + " &  will be delivered to the following address: " + address + ". See you soon!")
+    $("#message").show().text("Thanks for the order, " + name +  "! Your order cost $" + (newPizza.total() + newDrink.total()) + " &  will be delivered to the following address: " + address + ". See you soon!")
     });
 });
 
